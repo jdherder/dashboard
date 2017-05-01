563eaa59e9894db1af1abeafe4b17f9b4223c923
Josh Herder <jdherder@gmail.com>
2017-04-30 20:35:28 -0500

    Add additional background image assets. Set one by random on page load, hope to eventually make this configurable.

--- src/App.js
+++ src/App.js
@@ -5,8 +5,12 @@ import './App.css';
 
 class App extends Component {
   render() {
+
+    // TODO: Eventually, make backgrounds in general user configurable
+    const rand = Math.floor(Math.random() * 14) + 1;
+
     return (
-      <div className="App">
+      <div className="App" style={{backgroundImage: `url('/assets/${rand}.jpg')`}}>
         <Clock />
         <Weather />
       </div>
